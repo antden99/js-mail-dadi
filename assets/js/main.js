75bff2264ebd1948b9ddc6ead7308cb2771da675
antden99 <antoniodenigris@icloud.com>
2024-02-22 15:19:45 +0100

    add changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,20 +3,23 @@ console.log("tutto ok");
 
 //creo la mia lista di email
 const emailList = ["antonio@gmail.com", "francesco@gmail.com", "fabrizio@gmail.com"];
-console.log(emailList); //stampo in console per verificare
+//console.log(emailList); //stampo in console per verificare
 
 //creo una variabile a cui assegno l'input inserito nel prompt dall'utente
-const userInput = prompt("inserisci la tua email");
-console.log(userInput); //stampo in console per verificare
+let userInput = prompt("inserisci la tua email");
+//console.log(userInput); //stampo in console per verificare
+//console.log(typeof(userInput));
 
-
+//qui trasformo tutto in minuscolo per sicurezza che non ci sia un errore di caratteri
+userInput = userInput.toLowerCase();
 let cont = 0;
 
 //creo un for che scorra l'emailList(array)
 for (let index = 0; index < emailList.length; index++) {
+    
     const element = emailList[index];
     //controllo con l'IF se l'email è presente nella mia lista di emailList(array) e incremento cont;
-    if(element===userInput){
+    if(element===userInput.toLowerCase()){
            cont++;
     }
 }
