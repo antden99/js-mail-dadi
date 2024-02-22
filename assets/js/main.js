3656a0ddab4d17dffa7f9b999b0975dc8c21ca98
antden99 <antoniodenigris@icloud.com>
2024-02-22 14:32:04 +0100

    add for cicle

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,16 @@
 console.log("tutto ok");
 
 
+//creo la mia lista di email
+const emailList = ["antonio@gmail.com", "francesco@gmail.com", "fabrizio@gmail.com"];
+console.log(emailList); //stampo in console per verificare
 
+//creo una variabile a cui assegno l'input inserito nel prompt dall'utente
+const userInput = prompt("inserisci la tua email");
+console.log(userInput); //stampo in console per verificare
+
+//creo un for che scorra l'emailList(array)
+for (let index = 0; index < emailList.length; index++) {
+    const element = emailList[index];
+    
+}
