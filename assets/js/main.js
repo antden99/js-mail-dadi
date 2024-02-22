aa6b6e051ad1bd210d804c873419d3265867d6e0
antden99 <antoniodenigris@icloud.com>
2024-02-22 15:06:22 +0100

    add js logic and control

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,8 +9,20 @@ console.log(emailList); //stampo in console per verificare
 const userInput = prompt("inserisci la tua email");
 console.log(userInput); //stampo in console per verificare
 
+
+let cont = 0;
+
 //creo un for che scorra l'emailList(array)
 for (let index = 0; index < emailList.length; index++) {
     const element = emailList[index];
-    
+    //controllo con l'IF se l'email è presente nella mia lista di emailList(array) e incremento cont;
+    if(element===userInput){
+           cont++;
+    }
+}
+//controllo se cont è maggiore di 0, allora almeno una volta l'email è presente in lista, altrimenti no
+if(cont > 0){
+    console.log("l'email può accedere");
+}else {
+    console.log("l'email non può accedere")      
 }
