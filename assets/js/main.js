861746ed83dbf71618bfd951ce6a58ad4bdafe3c
antden99 <antoniodenigris@icloud.com>
2024-02-22 16:06:15 +0100

    add dadi game

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,16 +16,39 @@ let cont = 0;
 
 //creo un for che scorra l'emailList(array)
 for (let index = 0; index < emailList.length; index++) {
-    
+
     const element = emailList[index];
     //controllo con l'IF se l'email è presente nella mia lista di emailList(array) e incremento cont;
-    if(element===userInput.toLowerCase()){
-           cont++;
+    if (element === userInput.toLowerCase()) {
+        cont++;
     }
 }
 //controllo se cont è maggiore di 0, allora almeno una volta l'email è presente in lista, altrimenti no
-if(cont > 0){
+if (cont > 0) {
     console.log("l'email può accedere");
-}else {
-    console.log("l'email non può accedere")      
+} else {
+    console.log("l'email non può accedere")
+}
+
+
+///////////////GIOCO DADI//////////////////
+
+//creo due variabili, a cui assegno due numeri random che vanno da 1 a 6
+let giocatore = Math.floor(Math.random() * 6) + 1;
+let computer = Math.floor(Math.random() * 6) + 1;
+
+//stampo in console il valore dei numeri generati per controllo
+console.log(giocatore);
+console.log(computer);
+
+
+//creo un if, per vedere chi ha vinto o se il risultato finale è un pareggio
+if (giocatore > computer) {
+    console.log("l'utente ha vinto");
+    
+} else if (giocatore < computer) {
+    console.log("il computer ha vinto");
+}
+else if (giocatore == computer) {
+    console.log("pareggio");
 }
